810aadb33ac614efb9d30e0dccde07168eda6092
biuser <bi@sainsburysebooks.co.uk>
2015-02-03 13:35:58 +0000

    Fixed ls command with zero results.

--- hdfs.py
+++ hdfs.py
@@ -19,7 +19,7 @@ def test(path="", options=None):
         # add options to the command list
         command.insert(1, option)
 
-    std_out, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute__(command)
 
     return exit_code
 
@@ -32,12 +32,20 @@ def ls(path=None, output=None):
     if path:
         command.append(path)
 
-    std_out, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute__(command)
 
-    if output == 'stdout':
-        return std_out
+    if exit_code == 1:
+        # print std_err
+        return None
     else:
-        return std_out.split()[2::8][1:]
+        results = std_out.split()
+        if results[0] == "Found" and results[2] == "items":
+            del results[:3]
+
+        if output == 'stdout':
+            return std_out
+        else:
+            return results[7::8]
 
 
 def count(path, output=None):
@@ -45,7 +53,7 @@ def count(path, output=None):
     # command
     command = ["-count", path]
 
-    std_out, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute__(command)
 
     if output == 'stdout':
         return std_out
@@ -73,7 +81,7 @@ def mkdir(path, options=None):
         # add options to the command list
         command.insert(1, option)
 
-    std_out, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute__(command)
 
     return exit_code
 
@@ -97,7 +105,7 @@ def rm(path, options=None):
         # add options to the command list
         command.insert(1, option)
 
-    std_out, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute__(command)
 
     return exit_code
 
@@ -107,7 +115,7 @@ def mv(path_source, path_destination):
     # command
     command = ["-mv", path_source, path_destination]
 
-    std_out, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute__(command)
 
     return exit_code
 
@@ -129,7 +137,7 @@ def put(path_source, path_destination=None):
     else:
         command.append(".")
 
-    std_out, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute__(command)
 
     return exit_code
 
@@ -140,19 +148,16 @@ def __execute__(command):
     hdfs_command = ["hdfs", "dfs"]
     hdfs_command.extend(command)
 
-    print hdfs_command
-
     # run command
     hdfs_cmd = subprocess.Popen(hdfs_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     std_out, std_err = hdfs_cmd.communicate()
     exit_code = hdfs_cmd.returncode
 
-    if std_err and exit_code < 0:
-        # exit if there is an error
+    if exit_code < 0:
         print std_err
         raise SystemExit
 
-    return std_out, exit_code
+    return std_out, std_err, exit_code
 
 
 if __name__ == '__main__':
@@ -191,5 +196,6 @@ if __name__ == '__main__':
     print "\nHDFS Command: put"
     assert put("testfile") == 0
     assert put(["testfile1", "testfile2"]) == 0
+    print ls("testf*")
     rm("test*")
-
+    print ls("testf*")
