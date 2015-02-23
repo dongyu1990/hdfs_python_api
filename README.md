2afe5eaaf197280d7c3550df84f10cad90bfee22
biuser <bi@sainsburysebooks.co.uk>
2015-02-23 09:53:07 +0000

    Updated README.md file.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# HDFS Python API
+HDFS Python API
+===============
+
+A python interface to the shell commands that directly interact with the Hadoop Distributed File System (HDFS).
+
+Implementation
+==============
+
+To use the API, just import the `hdfs.py` file as per usual:
+
+```Python
+import hdfs
+```
+
+Commands
+========
+
+A full list of the hdfs shell commands and their usages can be found at the main site for [Apache Hadoop](http://hadoop.apache.org/docs/current/hadoop-project-dist/hadoop-common/FileSystemShell.html) . A list of the commands that have been added to the python api are shown below.
+
+**ls**
+
+hdfs.ls(path, output=None)
+
+path is either a string or list of strings. By default the output is a list of strings, to retrieve the raw output change the output option to *"stdout"*.
+
+```Python   
+hdfs.ls("temp")
+hdfs.ls("temp", output="stdout")
+```
--- hdfs.py
+++ hdfs.py
@@ -1,78 +1,31 @@
-#!/usr/bin/python
 
+import glob
 import subprocess
 
 
-def ls(path=None, output=None):
+def ls(path_hdfs_source=".", output=None):
 
     # command
     command = ["-ls"]
 
-    command.extend(__paths_type_check__(path))
+    command.extend(__paths_type_check(path_hdfs_source))
 
-    std_out, std_err, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute(command)
 
     if exit_code == 1:
-        return None
+        print std_err
+        return exit_code
     else:
-        results = std_out.split()
-        if results[0] == "Found" and results[2] == "items":
-            del results[:3]
-
-        if output == 'stdout':
+        if output == "stdout":
             return std_out
         else:
+            results = std_out.split()
+            if results[0] == "Found" and results[2] == "items":
+                del results[:3]
             return results[7::8]
 
 
-def count(path, output=None):
-
-    # command
-    command = ["-count"]
-
-    command.extend(__paths_type_check__(path))
-
-    std_out, std_err, exit_code = __execute__(command)
-
-    if output == 'stdout':
-        return std_out
-    else:
-        results = std_out.split()
-        return int(results[0]), int(results[1]), int(results[2]), results[3]
-
-
-def mv(path_source, path_destination):
-
-    # command
-    command = ["-mv"]
-
-    command.extend(__paths_type_check__(path_source))
-    command.extend(__paths_type_check__(path_destination))
-
-    std_out, std_err, exit_code = __execute__(command)
-
-    return exit_code
-
-
-def mkdir(paths, options=None):
-
-    # command
-    command = ["-mkdir"]
-
-    # command options
-    command_options = {"p": "-p"}
-
-    if options:
-        command.extend(__add_options__(options, command_options))
-
-    command.extend(__paths_type_check__(paths))
-
-    std_out, std_err, exit_code = __execute__(command)
-
-    return exit_code
-
-
-def rm(paths, options=None):
+def rm(path_hdfs_destination, options=None):
 
     # command
     command = ["-rm"]
@@ -82,33 +35,70 @@ def rm(paths, options=None):
                        "f": "-f"}
 
     if options:
-        command.extend(__add_options__(options, command_options))
+        command.extend(__add_options(options, command_options))
 
-    command.extend(__paths_type_check__(paths))
+    command.extend(__paths_type_check(path_hdfs_destination))
 
-    std_out, std_err, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute(command)
 
     return exit_code
 
 
-def put(paths_source, path_destination=None):
+def put(path_local_source, path_hdfs_destination=None, options=None):
+
+    if len(path_local_source) == 0:
+        print "no files selected to put on to HDFS:", path_local_source
+        return 1
 
     # command
     command = ["-put"]
 
-    command.extend(__paths_type_check__(paths_source))
+    # command options
+    command_options = {"f": "-f"}
 
-    if path_destination:
-        command.append(path_destination)
+    if options:
+        command.extend(__add_options(options, command_options))
+
+    command.extend(__paths_type_check(__expand(path_local_source)))
+
+    if path_hdfs_destination:
+        command.append(path_hdfs_destination)
     else:
         command.append(".")
 
-    std_out, std_err, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute(command)
 
     return exit_code
 
 
-def test(path="", options=None):
+def du(paths_hdfs, options=None, output=None):
+
+    # command
+    command = ["-du"]
+
+    # command options
+    command_options = {"s": "-s",
+                       "h": "-h"}
+
+    if options:
+        command.extend(__add_options(options, command_options))
+
+    command.extend(__paths_type_check(paths_hdfs))
+
+    std_out, std_err, exit_code = __execute(command)
+
+    if exit_code == 1:
+        print std_err
+        return exit_code
+    else:
+        if output == "stdout":
+            return std_out
+        else:
+            results = std_out.split("\n")[:-1]
+            return results
+
+
+def test(path_hdfs, options=None):
 
     # command
     command = ["-test"]
@@ -121,16 +111,71 @@ def test(path="", options=None):
                        "d": "-d"}
 
     if options:
-        command.extend(__add_options__(options, command_options))
+        command.extend(__add_options(options, command_options))
 
-    command.extend(__paths_type_check__(path))
+    command.extend(__paths_type_check(path_hdfs))
 
-    std_out, std_err, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute(command)
 
     return exit_code
 
 
-def tail(path, options=None):
+def count(path_hdfs, output=None):
+
+    # command
+    command = ["-count"]
+
+    command.extend(__paths_type_check(path_hdfs))
+
+    std_out, std_err, exit_code = __execute(command)
+
+    if output == 'stdout':
+        return std_out
+    else:
+        results = filter(None, [line.split() for line in std_out.split("\n")])
+        results = [(int(result[0]), int(result[1]), int(result[2]), result[3]) for result in results]
+
+        if len(results) > 1:
+            return results
+        else:
+            return results[0]
+
+
+def mv(path_hdfs_source, path_hdfs_destination):
+
+    # command
+    command = ["-mv"]
+
+    command.extend(__paths_type_check(path_hdfs_source))
+    command.extend(__paths_type_check(path_hdfs_destination))
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return exit_code
+
+
+def mkdir(path_hdfs_destination, options=None):
+
+    # command
+    command = ["-mkdir"]
+
+    # command options
+    command_options = {"p": "-p"}
+
+    if options:
+        command.extend(__add_options(options, command_options))
+
+    command.extend(__paths_type_check(path_hdfs_destination))
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return exit_code
+
+
+
+
+
+def tail(path_hdfs, options=None):
 
     # command
     command = ["-tail"]
@@ -139,28 +184,163 @@ def tail(path, options=None):
     command_options = {"f": "-f"}
 
     if options:
-        command.extend(__add_options__(options, command_options))
+        command.extend(__add_options(options, command_options))
 
-    command.extend(__paths_type_check__(path))
+    command.extend(__paths_type_check(path_hdfs))
 
-    std_out, std_err, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute(command)
 
     return exit_code
 
 
-def touchz(path):
+def touchz(*path_hdfs):
 
     # command
     command = ["-touchz"]
 
-    command.extend(__paths_type_check__(path))
+    command.extend(__paths_type_check(path_hdfs))
 
-    std_out, std_err, exit_code = __execute__(command)
+    std_out, std_err, exit_code = __execute(command)
 
     return exit_code
 
 
-def __paths_type_check__(paths):
+def stat(*path_hdfs):
+
+    # command
+    command = ["-stat"]
+
+    command.extend(__paths_type_check(path_hdfs))
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return std_out
+
+
+def get(path_hdfs_source, path_local_destination=".", crc=False, crc_check=False):
+
+    # command
+    command = ["-get"]
+
+    if crc:
+        command.append("-crc")
+
+    if crc_check:
+        command.append("-ignorecrc")
+
+    command.extend(__paths_type_check(path_hdfs_source))
+
+    command.append(path_local_destination)
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return exit_code
+
+
+def getmerge(path_hdfs_source, path_local_destination="."):
+
+    # command
+    command = ["-getmerge", __path_type_check(path_hdfs_source), __path_type_check(path_local_destination)]
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return exit_code
+
+
+def cp(paths_hdfs_source, path_hdfs_destination, options=None):
+
+    # command
+    command = ["-cp"]
+
+    # command options
+    command_options = {"f": "-f"}
+
+    if options:
+        command.extend(__add_options(options, command_options))
+
+    command.extend(__paths_type_check(paths_hdfs_source))
+
+    command.append(__path_type_check(path_hdfs_destination))
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return exit_code
+
+
+
+
+
+def cat(paths_hdfs):
+
+    # command
+    command = ["-cat"]
+
+    command.extend(__paths_type_check(paths_hdfs))
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return std_out
+
+
+def moveFromLocal(path_local_source, path_hdfs_destination="."):
+
+    # command
+    command = ["-moveFromLocal", __path_type_check(path_local_source), __path_type_check(path_hdfs_destination)]
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return exit_code
+
+
+def copyFromLocal(path_local_source, path_hdfs_destination=".", options=None):
+
+    # command
+    command = ["-copyFromLocal"]
+
+    # command options
+    command_options = {"f": "-f"}
+
+    if options:
+        command.extend(__add_options(options, command_options))
+
+    command.append(__path_type_check(path_local_source))
+
+    command.append(__path_type_check(path_hdfs_destination))
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return exit_code
+
+
+def copyToLocal(path_hdfs_source, path_local_destination=".", crc=False, crc_check=False):
+
+    # command
+    command = ["-copyToLocal"]
+
+    if crc:
+        command.append("-crc")
+
+    if crc_check:
+        command.append("-ignorecrc")
+
+    command.append(__path_type_check(path_hdfs_source))
+
+    command.append(__path_type_check(path_local_destination))
+
+    std_out, std_err, exit_code = __execute(command)
+
+    return exit_code
+
+
+def __path_type_check(path):
+
+    if isinstance(path, basestring):
+        return path
+    else:
+        raise TypeError("path must be a string", path)
+
+
+def __paths_type_check(paths):
 
     command = []
 
@@ -174,7 +354,20 @@ def __paths_type_check__(paths):
     return command
 
 
-def __add_options__(options, command_options):
+def __expand(paths):
+
+    expanded_paths = []
+
+    if isinstance(paths, basestring):
+        expanded_paths.extend(glob.glob(paths))
+    else:
+        for path in paths:
+            expanded_paths.extend(glob.glob(path))
+
+    return expanded_paths
+
+
+def __add_options(options, command_options):
 
     cmd_options = []
 
@@ -184,20 +377,20 @@ def __add_options__(options, command_options):
     return filter(None, cmd_options)
 
 
-def __execute__(command):
+def __execute(command):
 
     # hdfs command
     hdfs_command = ["hdfs", "dfs"]
     hdfs_command.extend(command)
 
-    print hdfs_command
+    print "command:", " ".join(hdfs_command)
 
     # run command
     hdfs_cmd = subprocess.Popen(hdfs_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     std_out, std_err = hdfs_cmd.communicate()
     exit_code = hdfs_cmd.returncode
 
-    if exit_code < 0:
+    if exit_code == 255:
         print std_err
         raise SystemExit
 
@@ -205,41 +398,4 @@ def __execute__(command):
 
 
 if __name__ == '__main__':
-
-    print "\nHDFS Command: test"
-    assert test("temp", "e") == 0
-    print "[x] passed true directory test"
-    assert test("abc", "e") == 1
-    print "[x] passed false directory test"
-
-    print "\nHDFS Command: ls"
-    assert len(ls("algorithms")) == 2
-    print "[x] passed true directory contents test"
-    assert type(ls("algorithms", output='stdout')) == str
-    print "[x] passed true directory stdout test"
-
-    print "\nHDFS Command: count"
-    print count("algorithms")
-
-    print "\nHDFS Command: mkdir"
-    assert mkdir("test") == 0
-    print "[x] passed true directory creation test"
-    assert mkdir("test1/test2", options="p") == 0
-    print "[x] passed true directory structure creation test"
-
-    print "\nHDFS Command: mv"
-    assert mv("test", "test_moved") == 0
-    print "[x] passed true move directory test"
-
-    print "\nHDFS Command: rm"
-    assert rm("test_moved", options="r") == 0
-    print "[x] passed delete directory test"
-    assert rm("test1", options="r") == 0
-    print "[x] passed delete directory structure test"
-
-    print "\nHDFS Command: put"
-    assert put("testfile") == 0
-    assert put(["testfile1", "testfile2"]) == 0
-    print ls("testf*")
-    rm("test*")
-    print ls("testf*")
+    pass
